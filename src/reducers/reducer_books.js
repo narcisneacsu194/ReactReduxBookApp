ed7c67dec89f502b699c40b5a3147af74476843a
Narcis Neacsu <narcisneacsu194@gmail.com>
2018-10-23 10:48:08 +0300

    Initial commit.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,6 @@
+export default function(){
+  return [{title: 'Javascript: The Good Parts', pages: 123},
+   {title: 'Harry Potter', pages: 50},
+    {title: 'The Dark Tower', pages: 110},
+     {title: 'Eloquent Ruby', pages: 1}];
+}
